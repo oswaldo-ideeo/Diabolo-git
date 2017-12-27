8ec1f26146ba622c19e00fe1525839432b1344a2
oswaldo-ideeo <oswaldo.castaneda@ideeo.mx>
2017-12-27 17:11:46 -0600

    aparecer y desaparecer experiencias

--- experiencias.php
+++ experiencias.php
@@ -1,70 +1,21 @@
+<?php $page = 'DIABOLO'; ?>
+<?php $desc = 'Aqui va la descripccion de la pagina'; ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="UTF-8">
-	<title>Diabolo</title>
-	<meta viewport>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Ideeo venta de equipos">
-	<meta name="author" content="Ideeo Oswaldo">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="css/style.css">
+	<?php include 'includes/head.php'; ?>
 </head>
 <body>
 
 	<div class="container-fluid back-diabolo">
-		<div class="container">
-			<nav class="nav-home row">
-				<div class="col-12 t-center show-xs">
-					<img src="images/diabolo-glow.png" alt="">
-				</div>				
-			</nav>
-
-			<div class="row justify-content-center">
-				<div class="col-sm-5 col-6">
-					<div class="row">
-						<div class="col-6">
-							<div class="menu-item t-diabolo">
-								<p class="no-space">HOME</p>
-								<p class="no-space fa fa-caret-up"></p>
-
-							</div>
-						</div>
-
-
-						<div class="col-6">
-							<div class="menu-item t-diabolo">
-								CALENDARIO
-							</div>
-						</div>
-
-
-					</div>					
-				</div>
-				<div class="col-sm-2 t-center reset hide-xs">
-					<img src="images/diabolo-glow.png" alt="">
-				</div>
-
-				<div class="col-sm-5 col-6 t-right">
-					<div class="menu-item t-diabolo">
-						<span class="fa fa-shopping-cart"></span>
-						<a href="" class="t-diabolo	">
-							<span>diablo66</span>
-							<span><img src="images/profile-icon.png" alt=""></span>
-							<span class="fa fa-caret-down"></span>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+		<?php include 'includes/header.php'; ?>
 
 		<div class="container">
 			<div class="row top-esp">
 				<div class="col-md-2">
-					<a href="perfil" class="text-select">Mis datos</a><br>
-					<a class="text-select" href="experiencias"><span>experiencias</span></a>
+					<a href="perfil.php" class="text-select">Mis datos</a><br>
+					<a class="text-select" href="experiencias.php"><span>experiencias</span></a>
 				</div>
 
 				<div class="col-md-10 back-negro">
@@ -85,7 +36,7 @@
 							<p style="color:#fff; font-size: 15px;">Activar el guardado de cuartadas</p>
 						</div>
 						<div class="col-sm-5">
-      						<button type="button" class="btn btn-lg btn-toggle active" data-toggle="button" aria-pressed="true" autocomplete="off">
+      						<button type="button" class="btn btn-lg btn-toggle" data-toggle="button" aria-pressed="false" autocomplete="off" id="onoff">
         						<div class="handle"></div>
       						</button>
     					</div>	
@@ -153,19 +104,7 @@
 
 
 
-		<div class="container footer">
-			<div class="row justify-content-center">
-				<div class="col-12 col-md-10 t-center">
-					<div class="row foot t-white">
-						<div class="col-sm-4 t-center">DIABOLO.COM&copy;</div>
-						<div class="col-sm-4 t-center"><a class="foot-link" style="color: rgba(255,255,255,0.6);" href="">TERMINOS Y CONDICIONES</a></div>
-						<div class="col-sm-4 t-center"><a class="foot-link" style="color: rgba(255,255,255,0.6);" href="">AVISO DE PRIVACIDAD</a></div>
-					</div>
-
-					
-				</div>
-			</div>
-		</div>
+		<?php include 'includes/footer.php'; ?>
 
 	</div>
 
@@ -193,7 +132,27 @@
 
 
 <script>
-	
+	var encendido = $('#onoff').attr('autocomplete')
+
+
+	if (encendido == 'off') {
+		$('.listado-experiencias').hide();
+	} else{
+		$('listado-experiencias').show();
+	}
+
+
+	$('#onoff').on('click', function(){
+		var apagado = $('#onoff').attr('aria-pressed');
+		if (apagado == 'true') {
+			$('.listado-experiencias').slideUp();
+		} else if (apagado == 'false') {
+			$('.listado-experiencias').slideDown();
+		}
+	})
+
+
+
 </script>
 
 </body>
