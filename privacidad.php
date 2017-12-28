f5143718b89d4d9ca91d582257135bb21dcaa0de
reatatype <fernando.perez@ideeo.mx>
2017-12-28 16:21:06 -0600

    terminos y avisos

---
+++ privacidad.php
@@ -0,0 +1,37 @@
+<?php $page = 'DIABOLO'; ?>
+<?php $desc = 'Aqui va la descripccion de la pagina'; ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include 'includes/head.php'; ?>
+	</head>
+	<body class="back-diabolo">
+		<div class="container">
+			<div class="col-sm-12 t-center" style="padding-top: 40px;">
+				<img src="images/diabolo-glow.png" alt="">
+			</div>
+			<div class="col-md-12 t-center aviso">
+				<img src="images/aviso.png"><br><br>
+				<p>El presente aviso de privacidad se establece en cumplimiento de lo dispuesto en la Ley Federal de Protección de Datos Personales en Posesión de los Particulares.</p>
+				<p>ALFA FOODS SA DE CV, con domicilio en Miguel Nieto # 660, Col. Centro en Monterrey, N.L., México CP 64000, es responsable de recabar sus datos personales, del uso que se le dé a los mismos y de su protección. Su información personal será utilizada para proveer los servicios y productos que ha solicitado, informarle sobre cambios en los mismos y evaluar la calidad del servicio que le brindamos.</p>
+				<p>Para las finalidades señaladas en el presente aviso de privacidad, podemos recabar sus datos personales de distintas formas: cuando usted nos los proporciona directamente; cuando visita nuestro sitio de Internet o utiliza nuestros servicios en línea, y cuando obtenemos información a través de otras fuentes que están permitidas por la ley. Recabamos sus datos personales de forma directa cuando usted mismo nos los proporciona por diversos medios, como cuando nos contacta o nos da información con objeto de que le prestemos un servicio. Los datos que obtenemos por este medio pueden ser, entre otros: Su nombre, correo electrónico (email), país y ciudad , páginas visitadas dentro de nuestro sitio, información para mejorar su experiencia en nuestro sitio como resolución de pantalla, explorador y tipo de sistema operativo utilizado. Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar el consentimiento que para tal fin nos haya otorgado, a través de los procedimientos que hemos implementado.</p>
+				<p>Aviso de Privacidad Detallado</p>
+				<p>En este Aviso de Privacidad se explica nuestra política respecto de la recolección, uso, divulgación y transmisión de los datos de usuarios por parte de ALFA FOODS SA DE CV. La presente política se podrá modificar a medida que actualizamos y ampliamos nuestros servicios. Por lo tanto, se recomienda consultarla periódicamente. Si accede a nuestro sitio web o de otro modo utiliza nuestro servicio, significa que presta su consentimiento a nuestras prácticas sobre información.</p>
+				<p>mportante: el presente Aviso de privacidad incluye hipervínculos a los que se puede acceder únicamente a través de nuestro sitio web, de modo que tendrá que visitar el sitio web para consultar los términos adicionales a los que se accede a través de los hipervínculos.</p>
+				<p>La presente política se podrá modificar a medida que actualizamos y ampliamos nuestros servicios. Consulte regularmente si existe alguna actualización a esta política. Usted acuerda que su aceptación de los Términos de Uso y el Aviso de privacidad posterior a su consentimiento original, así como el uso del sitio web de ALFA FOODS SA DE CV o el uso continuo de nuestro servicio después de publicar un cambio a esta política implica que acepta dichos cambios y se obliga a respetarlos. Podrá acceder a la última versión de esta Aviso de privacidad visitando nuestro sitio web y haciendo clic en “Aviso de privacidad” al pie de las páginas del sitio web de http://www.alfafoods.net</p>
+				<p>Para garantizar el correcto resguardo de sus datos personales y para dar pleno cumplimiento a las obligaciones que la LFPDPPP establece en la materia, se le informa que ALFA FOODS SA DE CV tiene implementadas las medidas de seguridad administrativas, técnicas y físicas necesarias y suficientes para evitar su daño, pérdida, alteración, destrucción, uso, acceso o divulgación indebida.</p>
+				<p>Los Datos Personales que han proporcionados serán conservados por un periodo de 1 año, excepto la que fiscalmente debe considerarse por 5 años, en medios físicos y electrónicos, y posteriormente descartados para evitar un tratamiento indebido de los mismos.</p>
+				<p>En ningún caso ALFA FOODS SA DE CV transferirá los datos personales de sus clientes a un tercero, sin el consentimiento previo de los titulares.</p>
+				<p>ROCEDIMIENTO PARA HACER VALER LOS DERECHOS ARCO.-</p>
+				<p>Usted tiene derecho de acceder a sus datos personales que poseemos y a los detalles del tratamiento de los mismos, así como a rectificarlos en caso de ser inexactos o incompletos; cancelarlos cuando considere que no se requieren para alguna de las finalidades señalados en el presente aviso de privacidad, estén siendo utilizados para finalidades no consentidas o haya finalizado la relación contractual o de servicio, o bien, oponerse al tratamiento de los mismos para fines específicos.</p>
+				<p>Los mecanismos que se han implementado para el ejercicio de dichos derechos, los cuales se conocen como derechos Arco mismos que se refieren a la rectificación, cancelación y oposición del Titular respecto al tratamiento de sus datos personales;</p>
+				<p>1) El procedimiento inicia con la presentación de la solicitud respectiva a los derechos Arco, en el domicilio de nuestra Oficina de Privacidad, mismo que fue debidamente señalado al principio del presente aviso de privacidad.</p>
+				<p>2) Su solicitud deberá contener la siguiente información:</p>
+				<p>Nombre del titular de los datos personales. Domicilio o cualquier otro medio de contacto. Documentos que acrediten su personalidad e identidad. La descripción clara y precisa de los datos personales. Cualquier otro documento que facilite la localización de los datos.</p>
+				<p></p>
+			
+			</div>
+		</div>
+	</body>
+</html> 
